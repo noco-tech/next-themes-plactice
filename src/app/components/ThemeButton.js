11fa7b5af23f42b327bf472ea8b8227e76f9e5f8
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-30 16:12:04 +0900

    スライドトグルボタン作成

--- src/app/components/ThemeButton.js
+++ src/app/components/ThemeButton.js
@@ -6,17 +6,33 @@ export default function ThemeButton() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
+
   useEffect(() => {
     setMounted(true);
   }, []);
 
   if (!mounted) return null;
 
+  const onClickHandler = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
+
   return (
-    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select>
+    <div>
+      {/* <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+        <option value="system">System</option>
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+      </select> */}
+
+      <div class="toggle_button">
+        <input value={theme} onClick={onClickHandler} id="toggle" class="toggle_input" type="checkbox" />
+        <label for="toggle" class="toggle_label" />
+      </div>
+    </div>
   );
 }
